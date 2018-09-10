e4847fb40cd778d597ec195d1175ae867085c085
Lewis Flude <lewisflude@Lewiss-MacBook-Pro.local>
2018-09-10 09:18:20 +0100

    Updates

--- path----0a20d577202490512cca.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{104:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Lewis Flude"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----0a20d577202490512cca.js.map
---
+++ sw.js
@@ -0,0 +1,84 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "webpack-runtime-1ccc357526562ff5bdc1.js"
+  },
+  {
+    "url": "app-f1fd4d8626304184d188.js"
+  },
+  {
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-74da3d5b47296991a9ec.js"
+  },
+  {
+    "url": "index.html",
+    "revision": "ac8200e234bbbac827947d172b66a147"
+  },
+  {
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "61a832a22352aedd93d722e7fac370ea"
+  },
+  {
+    "url": "component---src-pages-index-js.0cbef55a20b51d6dd15c.css"
+  },
+  {
+    "url": "component---src-pages-index-js-d900bbf02f082082b207.js"
+  },
+  {
+    "url": "0-15518b9851e4f6c36c31.js"
+  },
+  {
+    "url": "static/d/173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "component---src-pages-404-js.0cbef55a20b51d6dd15c.css"
+  },
+  {
+    "url": "component---src-pages-404-js-8eb1749eb509dbedc00e.js"
+  },
+  {
+    "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "55a9292126f8836e91e5d40b0a8cb54f"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/offline-plugin-app-shell-fallback/index.html", {
+  whitelist: [/^[^?]*([^.?]{5}|\.html)(\?.*)?$/],
+  blacklist: [/\?(.+&)?no-cache=1$/],
+});
+
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
